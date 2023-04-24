2f592a48c62d05bc08d91c117210dccc22674307
ManuBln <manu320cr@gmail.com>
2023-04-24 16:15:38 +0200

    Reescritura en python

--- PYTHON/Agenda.py
+++ PYTHON/Agenda.py
@@ -1,26 +1,5 @@
 import pymongo
 
-class Agenda:
-
-    def __init__(self, cliente, nombre_bd, nombre_coleccion):
-
-        self.cliente = cliente
-        self.bd = self.cliente[nombre_bd]
-        self.coleccion = self.db[nombre_coleccion]
-        self.contactos = []
-
-
-def insertar_contacto(self, nombre, apellidos, telefono, email):
-    documento = {"Nombre" : nombre , "Apellidos" : apellidos, "Telefono" : telefono, "Email" : email }
-
-
-
-
-
-
-
-
-
 #Establecer la conexion con la base de datos
 
 client = pymongo.MongoClient("mongodb://localhost:27017")
@@ -43,23 +22,3 @@ resultados = coleccion.find({"Nombre" : "Manuel"})
 
 
 
-while True:
-    print("1. Insertar Contacto")
-    print("2. Borrar Contacto")
-    print("3. Actualizar Contacto")
-    print("4. Salir")
-
-
-    opcion = input("Selecciona una opción: ")
-
-    if opcion == "1":
-        print("Has seleccionado la opción 1.")
-    elif opcion == "2":
-        print("Has seleccionado la opción 2.")
-    elif opcion == "3":
-         break
-    elif opcion == "4":
-        print("Adiós.")
-        break
-    else:
-        print("Opción inválida. Inténtalo de nuevo.")
